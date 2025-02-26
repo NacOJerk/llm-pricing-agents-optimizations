2e5b0e2322cb4dbc46b33efce4bf1c050514b275
Barr Kirel <barr.kirel@gmail.com>
2025-02-26 14:22:28 +0200

    Added a simple llm pricing agent template

---
+++ lllm_pricing_agent.py
@@ -0,0 +1,28 @@
+from typing import Any, Callable, Tuple
+from market_history import MarketHistory
+from pricing_agent import PricingAgent
+
+PromtContext = Any
+TextGenerator = Callable[[str], str]
+PromtGenerator = Callable[[int, MarketHistory, PromtContext], str]
+OutputParser = Callable[[str], Tuple[float, PromtContext]]
+
+class LLLMPricingAgent(PricingAgent):
+    def __init__(self, firm_id: float, price_per_unit: float,
+                text_generator: TextGenerator,
+                promt_generator: PromtGenerator,
+                output_parser: OutputParser,
+                initial_context: PromtContext = None):
+        super().__init__(firm_id, price_per_unit)
+        self.text_generator: TextGenerator = text_generator
+        self.promt_generator: PromtGenerator = promt_generator
+        self.output_parser: OutputParser = output_parser
+        self.context: PromtContext = initial_context
+    
+    def generate_price(self, market_history: MarketHistory) -> float:
+        generated_promt = self.promt_generator(self.get_firm_id(), market_history, self.context)
+        llm_output = self.text_generator(generated_promt)
+        new_price, new_context = self.output_parser(llm_output) # TODO: Allow throwing promt generation exception
+        self.context = new_context
+        return new_price
+
